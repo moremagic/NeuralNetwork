9ff99c11379860192ba4f47a5cb0fa584e865583
moremagic <itoumagic@gmail.com>
2016-02-12 19:49:22 +0900

    source edit

--- src/neuralnetwork/NeuralNetwork.java
+++ src/neuralnetwork/NeuralNetwork.java
@@ -4,76 +4,96 @@
  */
 package neuralnetwork;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author moremagic
  */
 public class NeuralNetwork {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        while(!Lerning_test()){
+        while (!Lerning_test()) {
             //nop
         }
     }
-    
+
+    /**
+     * 中間層を作成するユーティリティ
+     * @param inputCells 入力となるセルリスト
+     * @param size 作成する中間層
+     * @return 中間層セルリスト
+     */
+    public static List<INeuroCell> createCellTree(List<INeuroCell> inputCells, int size) {
+        List<INeuroCell> ret = new ArrayList<INeuroCell>();
+        for (int i = 0; i < size; i++) {
+            NeuroCell middoleCell = new NeuroCell();
+            for (INeuroCell in : inputCells) {
+                middoleCell.addInputCell(in);
+            }
+            ret.add(middoleCell);
+        }
+        return ret;
+    }
+
     public static boolean Lerning_test() {
         boolean ret;
         PlainNeuroCell x1 = new PlainNeuroCell(0d);
         PlainNeuroCell x2 = new PlainNeuroCell(0d);
-        
+
         NeuroCell rootCell = new NeuroCell();
-        
+
         //中間層を 3node作る
-        for(int i = 0 ; i < 3 ; i++){
-            NeuroCell middoleCell = new NeuroCell();
-            middoleCell.addInputCell(x1);
-            middoleCell.addInputCell(x2);
-            
-            rootCell.addInputCell(middoleCell);
+        for(INeuroCell cell: NeuralNetwork.createCellTree(NeuralNetwork.createCellTree(NeuralNetwork.createCellTree(Arrays.asList(new INeuroCell[]{x1, x2}), 3), 3), 3)){
+        //for(INeuroCell cell: NeuralNetwork.createCellTree(NeuralNetwork.createCellTree(Arrays.asList(new INeuroCell[]{x1, x2}), 3), 2)){
+        //for(INeuroCell cell: NeuralNetwork.createCellTree(Arrays.asList(new INeuroCell[]{x1, x2}), 3)){
+            rootCell.addInputCell(cell);
         }
-                
+
         double[][] datas = {
             {0, 0, 0},
             {0, 1, 1},
             {1, 0, 1},
-            {1, 1, 0},
-        };
-        
+            {1, 1, 0},};
+
         long cnt = 0L;
         double err = Double.MAX_VALUE;
-        
-        int ALPHA = 10; //学習係数
+
+        int ALPHA = 20; //学習係数
         double learnLimit = 0.001d; //目標値
-        
-        while(err > learnLimit){
+
+        while (err > learnLimit) {
             err = 0.0d;
-            for(int i = 0 ; i < datas.length ; i++){
+            for (int i = 0; i < datas.length; i++) {
                 x1.setValue(datas[i][0]);
-                x2.setValue(datas[i][1]);           
-                double ans = rootCell.firing();             
-                rootCell.learning(ans, datas[i][2], ALPHA);
+                x2.setValue(datas[i][1]);
+                double ans = rootCell.firing();
+                rootCell.learning(ans, datas[i][2], ALPHA);                
                 err += Math.pow((ans - datas[i][2]), 2);
             }
             ++cnt;
-            if(cnt % 100000 ==0){
+            if (cnt % 100000 == 0) {
                 //計算打ち切り
                 //計算に時間がかかりすぎるため再度計算しなおしたほうが良いと判断
-//                if(err > 0.5d){
+//                if (err > 0.3d) {
 //                    break;
 //                }
                 System.out.println("[" + cnt + "]" + err);
             }
         }
-        ret = ( err < learnLimit );
-        
+        ret = (err < learnLimit);
+
         //演算終了
-        if(!ret){
+        if (!ret) {
             System.out.println("not success: " + err);
-        }else{
+        } else {
             System.out.println("success!: " + err);
-            for(int i = 0 ; i < datas.length ; i++){
+            for (int i = 0; i < datas.length; i++) {
                 x1.setValue(datas[i][0]);
                 x2.setValue(datas[i][1]);
                 double ans = rootCell.firing();
@@ -81,36 +101,34 @@ public class NeuralNetwork {
             }
             System.out.println("root_cell_1: \n" + rootCell.toString());
         }
-        
+
         return ret;
     }
-    
-    
+
     public static void OrNeuroLerning() {
         PlainNeuroCell x1 = new PlainNeuroCell(0d);
         PlainNeuroCell x2 = new PlainNeuroCell(0d);
-        
+
         NeuroCell middoleCell_1 = new NeuroCellAsignete(1d);
         middoleCell_1.addInputCell(x1, -1d);
         middoleCell_1.addInputCell(x2, -3d);
-        
+
         NeuroCell middoleCell_2 = new NeuroCellAsignete(-2d);
         middoleCell_2.addInputCell(x1, 3d);
         middoleCell_2.addInputCell(x2, 1d);
-        
+
         NeuroCell rootCell = new NeuroCellAsignete(2);
         rootCell.addInputCell(middoleCell_1, -27);
         rootCell.addInputCell(middoleCell_2, 8);
-        
+
         double[][] datas = {
             {0, 0, 0},
             {0, 1, 1},
             {1, 0, 1},
-            {1, 1, 1},
-        };
-        
+            {1, 1, 1},};
+
         System.out.println("演算終了");
-        for(int i = 0 ; i < datas.length ; i++){
+        for (int i = 0; i < datas.length; i++) {
             x1.setValue(datas[i][0]);
             x2.setValue(datas[i][1]);
             double ans = rootCell.firing();
--- src/neuralnetwork/NeuroCell.java
+++ src/neuralnetwork/NeuroCell.java
@@ -7,105 +7,116 @@ package neuralnetwork;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * ニューロセル クラス
+ *
  * @author moremagic
  */
-public class NeuroCell implements INeuroCell{
+public class NeuroCell implements INeuroCell {
+
     public static final Random _RND_ = new Random(System.currentTimeMillis());
     private List<INeuroCell> m_inputCellArray = new ArrayList<INeuroCell>(); //親となるニューロセル
     private List<Double> m_waitArray = new ArrayList<Double>(); //ニューロセルの結合荷重
     double m_v = 0; //閾値
-    
-    public NeuroCell(){
+
+    public NeuroCell() {
         //インスタンス生成時に　重み係数を乱数を使って生成する
         m_v = (2d * _RND_.nextDouble() - 1d);
     }
-    
+
     @Override
-    public void addInputCell(INeuroCell cell){
+    public void addInputCell(INeuroCell cell) {
         m_inputCellArray.add(cell);
         m_waitArray.add(2d * _RND_.nextDouble() - 1d);
     }
-    
-    protected void addInputCell(INeuroCell cell, double w){
+
+    protected void addInputCell(INeuroCell cell, double w) {
         m_inputCellArray.add(cell);
         m_waitArray.add(w);
     }
 
     /**
      * ニューロセルの発火時 出力計算
-     * @return 
+     *
+     * @return
      */
     @Override
-    public double firing(){
+    public double firing() {
         double[] inputArray = new double[m_inputCellArray.size()];
         double[] waitArray = new double[m_inputCellArray.size()];
-        for(int i = 0 ; i < m_inputCellArray.size() ; i++){
+        for (int i = 0; i < m_inputCellArray.size(); i++) {
             inputArray[i] = m_inputCellArray.get(i).firing();
             waitArray[i] = m_waitArray.get(i);
         }
-        
+
         return NeuroCell.firing(inputArray, waitArray, m_v);
     }
-    
+public static long count = 0;
     /**
      * 学習
+     *
      * @param out 実際の出力
      * @param teachOut 教師値
      * @param alfa 学習係数
      */
     @Override
-    public void learning(double out, double teachOut, int alfa){
-        double d = ((teachOut - out) * out * (1-out));
-        for(int i = 0 ; i < m_waitArray.size() ; i++){
-            //結合荷重の学習
-            double cal = m_waitArray.get(i) + (alfa * m_inputCellArray.get(i).firing() * d);
+    public void learning(final double out, final double teachOut, final int alfa) {
+        //結合荷重の学習
+        //double E = teachOut - out;// 式 (21)
+        //double cal = m_waitArray.get(i) + (alfa * E * out * (1d - out) * m_inputCellArray.get(i).firing());// 式 (26)
+
+        //結合荷重の学習
+        double EE = alfa * (teachOut - out) * out * (1d - out);
+        for (int i = 0; i < m_waitArray.size(); i++) {
+            double cal = m_waitArray.get(i) + (EE * m_inputCellArray.get(i).firing());// 式 (26)
             m_waitArray.set(i, cal);
         }
-        
+
         // 閾値の学習
-        m_v += alfa *(-1.0)*d;
-        
+        m_v += alfa * (-1.0d) * (teachOut - out) * out * (1d - out);
+
         //バックプロパゲーション
-        for(INeuroCell cell : m_inputCellArray){
+        for (INeuroCell cell : m_inputCellArray) {
             cell.learning(out, teachOut, alfa);
         }
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("\t").append(this.getClass().getName()).append("\tw :");
-        for(double w: m_waitArray){
+        sb.append(this.getClass().getName()).append("\tw :");
+        for (double w : m_waitArray) {
             sb.append(w);
             sb.append(" ");
         }
-        sb.append("\n");
-        sb.append("\tv :").append(m_v).append("\n");
-        
-        for(INeuroCell cell: m_inputCellArray){
+        sb.append("\n").append("v :").append(m_v).append("\n");
+
+        sb.append("{\n");
+        for (INeuroCell cell : m_inputCellArray) {
             sb.append(cell.toString()).append("\n");
         }
+        sb.append("}\n");
+
         return sb.toString();
     }
-    
+
     /**
-     * ニューロセルの発火時 出力計算
-     * シグモイド関数による発火演算
-     * 
+     * ニューロセルの発火時 出力計算 シグモイド関数による発火演算
+     *
      * @param inputArray 入力値
      * @param waitArray 荷重係数
      * @param v 閾値
      * @return 出力
      */
-    public static double firing(double[] inputArray, double[] waitArray, double v){
+    public static double firing(double[] inputArray, double[] waitArray, double v) {
         double input = 0d;
-        for(int i = 0 ; i < inputArray.length ; i++){
+        for (int i = 0; i < inputArray.length; i++) {
             input += inputArray[i] * waitArray[i];
         }
         input -= v;
-        return 1/(1 + Math.exp(-input));
+        return 1 / (1 + Math.exp(-input));
     }
 }
